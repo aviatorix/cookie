87bd6a8d42b97df71bfabe3b2050b2b5685adc63
aviatorix <daviano.amaru@epipoli.com>
2020-09-01 11:19:19 +0200

    fix

--- docs/assets/flowe-bottom-bar/bottom-bar.js
+++ docs/assets/flowe-bottom-bar/bottom-bar.js
@@ -137,12 +137,28 @@ function setCookie(cname, cvalue) {
 }
 
 function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
+    /*var cookies = document.cookie.split(";");
 
     for (var i = 0; i < cookies.length; i++) {
         var cookie = cookies[i];
         var eqPos = cookie.indexOf("=");
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
         document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
+    }*/
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++)
+        eraseCookie(cookies[i].split("=")[0]);
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else var expires = "";
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
