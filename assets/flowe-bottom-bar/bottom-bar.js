e7bc87ad99cc2a49409c1708e76880ec6edd7cb8
aviatorix <daviano.amaru@epipoli.com>
2020-09-01 11:39:46 +0200

    Auto-generated commit

--- assets/flowe-bottom-bar/bottom-bar.js
+++ assets/flowe-bottom-bar/bottom-bar.js
@@ -38,10 +38,10 @@ class BottomBar extends HTMLElement {
 
     constructor() {
         super();
-    }
-    connectedCallback() {
         deleteAllCookies();
         localStorage.clear();
+    }
+    connectedCallback() {
         this.createComponent();
         let elements = document.getElementsByClassName('container-bar')[0];
         elements.classList.add("sticky");
@@ -137,14 +137,6 @@ function setCookie(cname, cvalue) {
 }
 
 function deleteAllCookies() {
-    /*var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }*/
     var cookies = document.cookie.split(";");
     for (var i = 0; i < cookies.length; i++)
         eraseCookie(cookies[i].split("=")[0]);
