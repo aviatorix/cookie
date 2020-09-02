a0b29064e88f46f7f14cad047d10d72c414261cd
aviatorix <daviano.amaru@epipoli.com>
2020-09-02 10:32:54 +0200

    Auto-generated commit

--- assets/flowe-bottom-bar/bottom-bar.js
+++ assets/flowe-bottom-bar/bottom-bar.js
@@ -137,9 +137,22 @@ function setCookie(cname, cvalue) {
     document.cookie = cname + "=" + cvalue + ";" + ";path=/";
 }
 
+/*
 function removeItem(sKey, sPath, sDomain) {
     document.cookie = encodeURIComponent(sKey) +
         "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
         (sDomain ? "; domain=" + sDomain : "") +
         (sPath ? "; path=" + sPath : "");
+}
+*/
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
 }
